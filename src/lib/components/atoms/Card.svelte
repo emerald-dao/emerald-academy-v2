5a2c0009c83aa15edcb1ab86ad18417b4b701349
mateoroldos <mateoroldos19@gmail.com>
2022-10-18 18:42:07 -0300

    feat: Card component

---
+++ src/lib/components/atoms/Card.svelte
@@ -0,0 +1,23 @@
+<script type="ts">
+	export let background: 'primary' | 'secondary' = 'secondary';
+	export let width: 'fit-content' | '100%' = 'fit-content';
+	export let padding: number = 4;
+	export let borderRadius: number = 1;
+</script>
+
+<div
+	class={`background-${background}`}
+	style="{`width: ${width}; padding: ${padding}rem; border-radius: ${borderRadius}rem`};"
+>
+	<slot />
+</div>
+
+<style type="scss">
+	.background-primary {
+		background-color: var(--clr-neutral-300);
+	}
+
+	.background-secondary {
+		background-color: var(--clr-neutral-900);
+	}
+</style>
