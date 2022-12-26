ed5937dd149b136dace6f784672b9d2599658acf
mateoroldos <mateoroldos19@gmail.com>
2022-12-26 16:54:03 -0300

    feat: MediaData validation

--- src/routes/catalog/bootcamps/[name]/components/generator-steps/MediaData.svelte
+++ src/routes/catalog/bootcamps/[name]/components/generator-steps/MediaData.svelte
@@ -1,6 +1,20 @@
 <script type="ts">
 	import { InputWrapper } from '@emerald-dao/component-library';
+	import mediaDataSuite from '../../validations/mediaDataSuite';
 	import StepButton from '../atoms/StepButton.svelte';
+
+	let formData = {
+		discordHandle: '',
+		bloctoAddress: ''
+	};
+
+	const handleChange = (input: Event) => {
+		const target = input.target as HTMLInputElement;
+
+		res = mediaDataSuite(formData, target.name);
+	};
+
+	let res = mediaDataSuite.get();
 </script>
 
 <div class="main-wrapper">
@@ -9,14 +23,38 @@
 	</div>
 	<div class="form-wrapper">
 		<form action="">
-			<label for="discord-handle">What's your Discord handle?</label>
-			<input name="discord-handle" type="text" placeholder="tsnakjake#233" />
+			<InputWrapper
+				name="discord-handle"
+				label="What's your Discord handle?"
+				errors={res.getErrors('discord-handle')}
+				isValid={res.isValid('discord-handle')}
+			>
+				<input
+					name="discord-handle"
+					type="text"
+					placeholder="tsnakjake#233"
+					bind:value={formData.discordHandle}
+					on:input={handleChange}
+				/>
+			</InputWrapper>
 
-			<label for="blocto-adress">What's your Blotco adress?</label>
-			<input name="blocto-adress" type="email" placeholder="0x99bd48c8035kdjeu504" />
+			<InputWrapper
+				name="blocto-address"
+				label="What's your Blocto address"
+				errors={res.getErrors('blocto-address')}
+				isValid={res.isValid('blocto-address')}
+			>
+				<input
+					name="blocto-address"
+					type="text"
+					placeholder="0x99bd48c8035kdjeu504"
+					bind:value={formData.bloctoAddress}
+					on:input={handleChange}
+				/>
+			</InputWrapper>
 		</form>
 	</div>
-	<StepButton active={true} />
+	<StepButton active={res.isValid()} />
 </div>
 
 <style type="scss">
---
+++ src/routes/catalog/bootcamps/[name]/validations/mediaDataSuite.ts
@@ -0,0 +1,23 @@
+import { create, enforce, test, only } from 'vest';
+
+const mediaDataSuite = create((data = {}, currentField) => {
+	only(currentField);
+
+	test('discord-handle', 'Discord handle should be shorter than 20 chars', () => {
+		enforce(data.discordHandle).shorterThan(30);
+	});
+
+	test('discord-handle', 'Discord handle is required', () => {
+		enforce(data.discordHandle).isNotBlank();
+	});
+
+	test('blocto-address', 'Blocto address is required', () => {
+		enforce(data.bloctoAddress).isNotBlank();
+	});
+
+	test('blocto-address', 'Blocto address should be 18 chars long', () => {
+		enforce(data.bloctoAddress).lengthEquals(18);
+	});
+});
+
+export default mediaDataSuite;
