e98f5ba9a6483ab743cb88341a12ab7c298bf757
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-01-30 13:26:36 -0300

    Cadence By Example corrections

--- src/routes/cadence-by-example/[id]/+layout.svelte
+++ src/routes/cadence-by-example/[id]/+layout.svelte
@@ -16,33 +16,35 @@
 	<slot />
 	<div class="adjacents-wrapper">
 		{#if previousExample}
-			<a href={`/cadence-by-example/${previousExample.path.split('/')[3]}`}>
-				<div class="each-adjacent-wrapper">
-					<Icon icon="tabler:arrow-left" style="color: var(--clr-text-main);" />
-					<div class="left-wrapper column-2">
-						<p class="heading w-medium">
-							{previousExample.meta.title}
-						</p>
-						<p class="text small">
-							{previousExample.meta.excerpt}
-						</p>
-					</div>
+			<a
+				class="step-back each-adjacent-wrapper"
+				href={`/cadence-by-example/${previousExample.path.split('/')[3]}`}
+			>
+				<Icon icon="tabler:arrow-left" style="color: var(--clr-text-main);" />
+				<div class="left-wrapper column-2">
+					<p class="heading w-medium">
+						{previousExample.meta.title}
+					</p>
+					<p class="text small">
+						{previousExample.meta.excerpt}
+					</p>
 				</div>
 			</a>
 		{/if}
 		{#if nextExample}
-			<a href={`/cadence-by-example/${nextExample.path.split('/')[3]}`}>
-				<div class="each-adjacent-wrapper right">
-					<div class="column-2">
-						<p class="heading w-medium">
-							{nextExample.meta.title}
-						</p>
-						<p class="text small">
-							{nextExample.meta.excerpt}
-						</p>
-					</div>
-					<Icon icon="tabler:arrow-right" style="color: var(--clr-text-main);" />
+			<a
+				class="step-next each-adjacent-wrapper"
+				href={`/cadence-by-example/${nextExample.path.split('/')[3]}`}
+			>
+				<div class="column-2">
+					<p class="heading w-medium">
+						{nextExample.meta.title}
+					</p>
+					<p class="text small">
+						{nextExample.meta.excerpt}
+					</p>
 				</div>
+				<Icon icon="tabler:arrow-right" style="color: var(--clr-text-main);" />
 			</a>
 		{/if}
 	</div>
@@ -55,18 +57,34 @@
 		margin-top: var(--space-11);
 
 		@include mq(medium) {
-			justify-content: space-between;
-			flex-direction: row;
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			grid-template-areas: 'start end';
+			gap: var(--space-4);
 		}
 
 		a {
 			text-decoration: none;
 		}
 
+		.step-back {
+			grid-area: start;
+		}
+
+		.step-next {
+			grid-area: end;
+			margin-top: var(--space-4);
+
+			@include mq(medium) {
+				margin-top: 0;
+			}
+		}
+
 		.each-adjacent-wrapper {
 			display: flex;
 			flex-direction: row;
 			align-items: center;
+			justify-content: space-between;
 			gap: var(--space-5);
 			padding: var(--space-4) var(--space-5);
 			border-radius: var(--radius-2);
@@ -74,15 +92,6 @@
 
 			.left-wrapper {
 				text-align: end;
-			}
-		}
-
-		.right {
-			margin-top: var(--space-4);
-			justify-content: end;
-
-			@include mq(medium) {
-				margin-top: 0;
 			}
 		}
 	}
