beb2e92d328a1e0c7667d2d49e01a2b49009ed5c
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-01-30 13:24:14 -0300

    Templates adjustments

--- src/routes/templates/[name]/+page.ts
+++ src/routes/templates/[name]/+page.ts
@@ -3,10 +3,12 @@ import { fetchGithubUser } from "$lib/utilities/api/githubApi/fetchGithubUser";
 export const load = async ({ params }) => {
 	try {
 		const overviewFile = await import(`../../../lib/content/templates/${params.name}/overview.ts`);
+		const readmeFile = await import(`../../../lib/content/templates/${params.name}/readme.md`);
 		const githubUser = await fetchGithubUser(overviewFile.overview.user.github)
-		
+	
 		return {
 			overview: overviewFile.overview,
+			readme: readmeFile.default,
 			githubUser: githubUser.json
 		};
 	} catch (e) {
