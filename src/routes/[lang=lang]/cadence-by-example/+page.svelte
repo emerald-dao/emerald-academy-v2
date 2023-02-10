46227594f55e178223a7b7398e880015b6c0bc5e
mateoroldos <mateoroldos19@gmail.com>
2023-02-10 15:21:17 -0300

    feat: CadenceByExample improvements

--- src/routes/[lang=lang]/cadence-by-example/+page.svelte
+++ src/routes/[lang=lang]/cadence-by-example/+page.svelte
@@ -1,91 +1,106 @@
 <script type="ts">
+	import { InputWrapper } from '@emerald-dao/component-library';
 	import { locale, LL } from '$i18n/i18n-svelte';
+	import type { MarkdownMeta } from '$lib/types/content/metadata/markdown-meta.interfaces';
 	import { createSearchStore, searchHandler } from '$stores/searchBar';
 	import { onDestroy } from 'svelte';
 
-	export let data;
+	export let data: Data;
 
-	type CadenceByExample = {
-		meta: Record<PropertyKey, any> = {};
+	interface Data {
+		content: CadenceByExample[];
+		locale: string;
+	}
+
+	interface CadenceByExample {
+		meta: MarkdownMeta;
 		path: string;
-		searchTerms: string;
-	};
+	}
 
-	const searchCadence: CadenceByExample[] = data.content.map((product: CadenceByExample) => ({
-		...product,
-		searchTerms: `${product.meta.title}`
+	$: searchCadence = data.content.map((example: CadenceByExample) => ({
+		...example,
+		searchTerms: `${example.meta.title}`
 	}));
 
-	const searchStore = createSearchStore(searchCadence);
+	$: searchStore = createSearchStore(searchCadence);
 
-	const unsubscribe = searchStore.subscribe((model) => searchHandler(model));
+	$: unsubscribe = searchStore.subscribe((model) => searchHandler(model));
 
 	onDestroy(() => {
 		unsubscribe();
 	});
 </script>
 
-<section class="container">
-	<div class="sidebar">
-		<h5>{$LL.SEARCH()}</h5>
-		<input type="text" placeholder="Search..." bind:value={$searchStore.search} />
-	</div>
-	<div class="main">
-		<h1>Cadence by Example</h1>
-		{#each $searchStore.filtered as content}
-			<a
-				class="title-wrapper w-medium heading"
-				href={`/${$locale}/cadence-by-example/${content.path.split('/')[3]}`}
-				>{content.meta.title}</a
-			>
-		{/each}
+<section class="container-small column-10">
+	<h1>Cadence by Example</h1>
+	<div class="main-wrapper">
+		<div class="sidebar">
+			<h5>{$LL.SEARCH()}</h5>
+			<InputWrapper name="search" errors={[]} isValid={false} icon="tabler:search">
+				<input type="text" placeholder="Search..." bind:value={$searchStore.search} />
+			</InputWrapper>
+		</div>
+		<div class="main">
+			{#if $searchStore.search.length > 0 && $searchStore.filtered.length === 0}
+				<p>No results found</p>
+			{/if}
+			{#each $searchStore.filtered as content, i}
+				<a
+					class="card heading"
+					href={`/${$locale}/cadence-by-example/${content.path.split('/')[3]}`}
+					>{`${i + 1}. ${content.meta.title}`}</a
+				>
+			{/each}
+		</div>
 	</div>
 </section>
 
 <style type="scss">
 	section {
-		display: flex;
-		flex-direction: column;
-		gap: var(--space-5);
-
-		@include mq(small) {
-			display: grid;
-			grid-template-columns: 1fr 3fr;
-			gap: var(--space-10);
-		}
-
-		.sidebar {
-			display: flex;
-			flex-direction: column;
-			border-bottom: var(--border-width-primary) var(--clr-border-primary) solid;
-			padding-bottom: var(--space-5);
-
-			@include mq(small) {
-				border-right: var(--border-width-primary) var(--clr-border-primary) solid;
-				border-bottom: 0;
-				height: fit-content;
-				padding-block: var(--space-9);
-				padding-right: var(--space-10);
-				position: sticky;
-				top: 50px;
-				gap: 0;
-			}
-
-			h5 {
-				margin: var(--space-5) 0;
-			}
-		}
-
-		.main {
+		.main-wrapper {
 			display: flex;
 			flex-direction: column;
 			gap: var(--space-5);
 
-			.title-wrapper {
-				text-decoration: none;
-				border: var(--border-width-primary) solid var(--clr-border-primary);
-				border-radius: var(--radius-5);
-				padding: var(--space-4) var(--space-6);
+			@include mq(small) {
+				display: grid;
+				grid-template-columns: 1fr 3fr;
+				gap: var(--space-10);
+			}
+			.sidebar {
+				display: flex;
+				flex-direction: column;
+				border-bottom: var(--border-width-primary) var(--clr-border-primary) solid;
+				padding-bottom: var(--space-5);
+
+				@include mq(small) {
+					border-right: var(--border-width-primary) var(--clr-border-primary) solid;
+					border-bottom: 0;
+					height: fit-content;
+					padding-bottom: var(--space-8);
+					padding-right: var(--space-10);
+					position: sticky;
+					top: 100px;
+					gap: 0;
+				}
+
+				h5 {
+					font-size: var(--font-size-4);
+					margin-bottom: var(--space-2);
+					margin-top: 0;
+				}
+			}
+
+			.main {
+				display: flex;
+				flex-direction: column;
+				gap: var(--space-5);
+
+				.card {
+					text-decoration: none;
+					padding: var(--space-4) var(--space-6);
+					transition: 0.3s;
+				}
 			}
 		}
 	}
