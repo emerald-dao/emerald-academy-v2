46b6e8b0f6db75d558935f3256ed7d453d0344c0
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-02-15 10:50:31 -0300

    emailjs arrangements

--- src/lib/stores/onBoarding/OnBoardingSteps.ts
+++ src/lib/stores/onBoarding/OnBoardingSteps.ts
@@ -6,6 +6,27 @@ import Join from '../../../routes/[lang=lang]/catalog/bootcamps/[name]/__compone
 import MediaData from '../../../routes/[lang=lang]/catalog/bootcamps/[name]/__components/generator-steps/MediaData.svelte';
 import ThanksMessage from '../../../routes/[lang=lang]/catalog/bootcamps/[name]/__components/generator-steps/ThanksMessage.svelte';
 import { createActiveStep } from '$stores/custom/steps/ActiveStep';
+import emailjs from '@emailjs/browser';
+import { createBootcampOnboardingStore } from '$stores/BootcampOnboardingStore';
+import { get } from 'svelte/store';
+
+const sendEmail = async () => {
+	emailjs
+		.send(
+			'service_v2qa1om',
+			'template_j0mkz6j',
+			get(createBootcampOnboardingStore),
+			'IGzpHwMNeQiWCm1_R'
+		)
+		.then(
+			(result) => {
+				console.log('SUCCESS!', result.text);
+			},
+			(error) => {
+				console.log('FAILED...', error.text);
+			}
+		);
+};
 
 export const onBoardingSteps = createSteps([
 	{ title: 'Welcome', slug: 'welcome', component: WelcomeAboard, action: null, form: true },
@@ -24,7 +45,7 @@ export const onBoardingSteps = createSteps([
 		form: false
 	},
 	{ title: 'Join', slug: 'Join', component: Join, action: null, form: false },
-	{ title: 'MediaData', slug: 'MediaData', component: MediaData, action: null, form: false },
+	{ title: 'MediaData', slug: 'MediaData', component: MediaData, action: sendEmail, form: false },
 	{
 		title: 'ThanksMessage',
 		slug: 'ThanksMessage',
