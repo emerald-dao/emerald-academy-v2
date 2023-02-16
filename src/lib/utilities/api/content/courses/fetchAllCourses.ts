4a088acaec9c72e8705c1dad2d7dc675eb0e5310
mateoroldos <mateoroldos19@gmail.com>
2023-02-16 12:24:49 -0300

    fix: Improve fetch functions

--- src/lib/utilities/api/content/courses/fetchAllCourses.ts
+++ src/lib/utilities/api/content/courses/fetchAllCourses.ts
@@ -1,61 +1,49 @@
 import type { Locales } from '$i18n/i18n-types';
 
 export const fetchAllCourses = async (locale: Locales) => {
-	console.log('fetchAllCourses');
-	console.log('locale', locale);
-
 	const overviews = import.meta.glob('/src/lib/content/courses/**/**/*.ts');
 	const contents = import.meta.glob('/src/lib/content/courses/**/**/**/*.md');
 
 	const iterableOverviews = Object.entries(overviews);
 	const iterableContents = Object.entries(contents);
 
-	console.log('o', iterableOverviews);
-	console.log('a', iterableContents);
+	const thisLangOverviews = iterableOverviews.filter(([path]) => {
+		return path.split('/')[6] == locale;
+	});
+
+	const thisLangContents = iterableContents.filter(([path]) => {
+		return path.split('/')[6] == locale;
+	});
 
 	const allCourses = await Promise.all(
-		iterableOverviews.map(async ([path, resolver]) => {
-			const thisCourseLang = path.split('/')[6];
-
-			console.log('thisCourseLang', thisCourseLang);
-
-			if (thisCourseLang != locale) {
-				console.log('jueeeera');
-
-				return;
-			}
-
+		thisLangOverviews.map(async ([path, resolver]) => {
 			const { overview } = await resolver();
+
+			const thisCourseContent = thisLangContents.filter(([path]) => {
+				return path.includes(overview.slug);
+			});
 
 			const contents = {};
 
 			await Promise.all(
-				iterableContents.map(async ([path, resolver]) => {
+				thisCourseContent.map(async ([path, resolver]) => {
 					const { metadata } = await resolver();
 
-					// console.log('path', path);
-					// console.log('overview.slug', overview.slug);
+					const parts = path.split('/');
+					const week = parts[parts.length - 2];
 
-					if (path.includes(overview.slug)) {
-						const parts = path.split('/');
-						const week = parts[parts.length - 2];
-
-						if (!contents[week]) {
-							contents[week] = [];
-						}
-
-						const slug = parts.slice(3, 8).join('/').replace('content', 'catalog');
-
-						contents[week].push({
-							slug,
-							metadata
-						});
+					if (!contents[week]) {
+						contents[week] = [];
 					}
+
+					const slug = parts.slice(3, 9).join('/').replace('content', 'catalog').replace('.md', '');
+
+					contents[week].push({
+						slug,
+						metadata
+					});
 				})
 			);
-
-			console.log('contentsssssss', contents);
-			console.log('overviewwwwww', overview);
 
 			return {
 				overview,
