15cdbe420078fb2198ee24ce43866e7a394bc064
mateoroldos <mateoroldos19@gmail.com>
2023-02-17 09:56:14 -0300

    feat: Connect new Header with Avatar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,21 +6,20 @@
 	import '@emerald-dao/component-library/styles/app.scss';
 	import '$lib/styles/_articles.scss';
 	import '$lib/styles/_code.scss';
-	import { page } from '$app/stores';
-	import LL, { setLocale, locale } from '$i18n/i18n-svelte';
+	import { setLocale, locale } from '$i18n/i18n-svelte';
 	import HeadHrefLangs from '$lib/components/i18n/HeadHrefLangs.svelte';
 	import type { LayoutData } from './$types';
 	import { Header, Footer } from '@emerald-dao/component-library';
-	// import { navElements, emeraldTools, socialMedia } from '$lib/config/navigation';
 	import { emeraldTools, socialMedia } from '$lib/config/navigation';
 	import { theme } from '$stores/ThemeStore';
 	import { logIn, unauthenticate } from '$flow/actions';
-	import { user } from '$stores/FlowStore';
+	import { network, user } from '$stores/FlowStore';
 	import { getFindProfile } from '$flow/utils';
 	import LocaleSwitcher from '$lib/components/i18n/LocaleSwitcher.svelte';
 	import dappInfo from '$lib/config/config';
 
 	export let data: LayoutData;
+
 	// at the very top, set the locale before you access the store and before the actual rendering takes place
 	setLocale(data.locale);
 
@@ -40,20 +39,33 @@
 			url: `/${$locale}/resources`,
 			prefetch: true
 		},
-		// {
-		// 	name: 'Achievements',
-		// 	url: `/${$locale}/achievements`,
-		// 	prefetch: true
-		// },
 		{
 			name: 'Templates',
 			url: `/${$locale}/templates`,
 			prefetch: true
 		}
 	];
+
+	export const avatarDropdownNav = [
+		{
+			name: 'My Achievements',
+			url: `/${$locale}/achievements`,
+			prefetch: true,
+			icon: 'tabler:diamond'
+		}
+	];
 </script>
 
-<Header themeStore={theme} {logIn} {unauthenticate} {getFindProfile} {navElements} user={$user}>
+<Header
+	themeStore={theme}
+	{logIn}
+	{unauthenticate}
+	{getFindProfile}
+	{navElements}
+	user={$user}
+	network={$network}
+	avatarDropDownNavigation={avatarDropdownNav}
+>
 	<LocaleSwitcher slot="commands" />
 </Header>
 <main>
