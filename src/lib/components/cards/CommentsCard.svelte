f7386676213df58eb4ebe58bae761b93e7739afc
mateoroldos <mateoroldos19@gmail.com>
2023-02-17 12:32:39 -0300

    fix: Make socials infinite

--- src/lib/components/cards/CommentsCard.svelte
+++ src/lib/components/cards/CommentsCard.svelte
@@ -19,52 +19,50 @@
 	</div>
 	<div class="data">
 		<img src="/Jacob.jpeg" alt="avatar" class="image" />
-
 		<div class="name">
 			<p>{comment.name}</p>
 			<p>{comment.username}</p>
 		</div>
 	</div>
-	<div class="social-media">
-		<p class="media">{comment.socialMedia}</p>
-	</div>
+	<div class="social-media" />
 </div>
 
 <style type="scss">
-	.data {
-		display: flex;
-		flex-direction: row;
-		align-items: left;
-	}
-	.name {
-		display: flex;
-		flex-direction: column;
-		margin: var(--space-3);
-	}
-	p {
-		font-family: Inter;
-		color: var(--clr-neutral-600);
-	}
-	.quotation-marks {
-		font-size: var(--font-size-10);
-		font-family: Inter;
-		font-weight: var(--font-weight-bold);
-	}
-	.image {
-		height: 50px;
-		width: 50px;
-		background-color: #bbb;
-		border-radius: 50%;
-		display: inline-block;
-		margin: var(--space-3) 0 var(--space-3) 0;
-	}
-	.social-media {
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-end;
-	}
-	.media {
-		font-family: JetBrains Mono;
-		font-weight: var(--font-normal);
+	.card-primary {
+		background-color: var(--clr-background-secondary);
+		.data {
+			display: flex;
+			flex-direction: row;
+			align-items: left;
+
+			.name {
+				display: flex;
+				flex-direction: column;
+				margin: var(--space-3);
+			}
+
+			.image {
+				height: 50px;
+				width: 50px;
+				background-color: var(--clr-surface-primary);
+				border-radius: 50%;
+				display: inline-block;
+				margin: var(--space-3) 0 var(--space-3) 0;
+			}
+		}
+		p {
+			font-family: Inter;
+		}
+		.quotation-marks {
+			font-size: var(--font-size-10);
+			font-family: Inter;
+			font-weight: var(--font-weight-bold);
+		}
+
+		.social-media {
+			display: flex;
+			flex-direction: row;
+			justify-content: flex-end;
+		}
 	}
 </style>
