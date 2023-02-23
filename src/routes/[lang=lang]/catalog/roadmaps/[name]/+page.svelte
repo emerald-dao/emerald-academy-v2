ecdfbe98ff33e24bbc5ca664517288c571287523
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-02-23 12:49:28 -0300

    /roadmaps/[name] corrections

--- src/routes/[lang=lang]/catalog/roadmaps/[name]/+page.svelte
+++ src/routes/[lang=lang]/catalog/roadmaps/[name]/+page.svelte
@@ -1,6 +1,5 @@
 <script type="ts">
 	import RoadmapContentCard from './__components/RoadmapContentCard.svelte';
-	import CurriculumOverview from '$lib/components/cards/CurriculumOverview.svelte';
 	import SpecificContentCard from '$lib/components/cards/ContentIntro.svelte';
 	import type { RoadmapOverview } from '$lib/types/content/roadmap.interface';
 
@@ -12,17 +11,13 @@
 </script>
 
 <section>
-	<div class="container main-wrapper">
+	<div class="container-small main-wrapper">
 		<div class="content">
 			<SpecificContentCard overview={data.roadmap} />
 		</div>
-
-		<div class="overview-wrapper">
-			<CurriculumOverview overview={data.roadmap} />
-		</div>
 	</div>
 	<section>
-		<div class="container">
+		<div class="container-small">
 			{#each data.roadmap.contents as roadmapContent}
 				<RoadmapContentCard {roadmapContent} />
 			{/each}
