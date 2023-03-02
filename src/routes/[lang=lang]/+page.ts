96d0dc216648180a75eff87f4e82e027fa193b22
mateoroldos <mateoroldos19@gmail.com>
2023-03-02 13:49:35 -0300

    fix: HomePage ContentCard links

--- src/routes/[lang=lang]/+page.ts
+++ src/routes/[lang=lang]/+page.ts
@@ -6,21 +6,33 @@ export const load: PageLoad = async ({ params }) => {
 		const featuredCourse1 = await import(
 			`../../lib/content/courses/${featuredContent.courses[0]}/${params.lang}/overview.ts`
 		);
+		featuredCourse1.overview.slug = `courses/${featuredContent.courses[0]}`;
+
 		const featuredCourse2 = await import(
 			`../../lib/content/courses/${featuredContent.courses[1]}/${params.lang}/overview.ts`
 		);
+		featuredCourse2.overview.slug = `courses/${featuredContent.courses[1]}`;
+
 		const featuredBootcamp1 = await import(
 			`../../lib/content/bootcamps/${featuredContent.bootcamps[0]}/en/overview.ts`
 		);
+		featuredBootcamp1.overview.slug = `bootcamps/${featuredContent.courses[0]}`;
+
 		const featuredBootcamp2 = await import(
 			`../../lib/content/bootcamps/${featuredContent.bootcamps[1]}/en/overview.ts`
 		);
+		featuredBootcamp2.overview.slug = `bootcamps/${featuredContent.courses[1]}`;
+
 		const featuredRoadmap1 = await import(
 			`../../lib/content/roadmaps/${featuredContent.roadmaps[0]}/${params.lang}/overview.ts`
 		);
+		featuredRoadmap1.overview.slug = `roadmaps/${featuredContent.courses[0]}`;
+
 		const featuredRoadmap2 = await import(
 			`../../lib/content/roadmaps/${featuredContent.roadmaps[1]}/${params.lang}/overview.ts`
 		);
+		featuredRoadmap2.overview.slug = `roadmaps/${featuredContent.courses[1]}`;
+
 		return {
 			courses: [featuredCourse1.overview, featuredCourse2.overview],
 			bootcamps: [featuredBootcamp1.overview, featuredBootcamp2.overview],
