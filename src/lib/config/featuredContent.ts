3e785c6f17e33726011c20c17b65b1e6e0d8f78d
mateoroldos <mateoroldos19@gmail.com>
2023-03-02 13:04:47 -0300

    fix: Homepage imports

--- src/lib/config/featuredContent.ts
+++ src/lib/config/featuredContent.ts
@@ -1,5 +1,5 @@
 export const featuredContent = {
-	courses: ['courses/beginner-cadence', 'courses/beginner-dapp'],
-	bootcamps: ['bootcamps/basic-dapp', 'bootcamps/basic-dapp'],
-	roadmaps: ['roadmaps/basic-dapp', 'roadmaps/beginner-cadence']
+	courses: ['beginner-cadence', 'beginner-dapp'],
+	bootcamps: ['basic-dapp', 'basic-dapp'],
+	roadmaps: ['basic-dapp', 'beginner-cadence']
 };
--- src/routes/[lang=lang]/+page.ts
+++ src/routes/[lang=lang]/+page.ts
@@ -3,34 +3,28 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params }) => {
 	try {
-		const courseOverviewFile = await import(
-			/* @vite-ignore */
-			`../../lib/content/${featuredContent.courses[0]}/${params.lang}/overview`
+		const featuredCourse1 = await import(
+			`../../lib/content/courses/${featuredContent.courses[0]}/${params.lang}/overview.ts`
 		);
-		const courseOverviewFile2 = await import(
-			/* @vite-ignore */
-			`../../lib/content/${featuredContent.courses[1]}/${params.lang}/overview`
+		const featuredCourse2 = await import(
+			`../../lib/content/courses/${featuredContent.courses[1]}/${params.lang}/overview.ts`
 		);
-		const courseOverviewFile3 = await import(
-			/* @vite-ignore */
-			`../../lib/content/${featuredContent.bootcamps[0]}/en/overview`
+		const featuredBootcamp1 = await import(
+			`../../lib/content/bootcamps/${featuredContent.bootcamps[0]}/en/overview.ts`
 		);
-		const courseOverviewFile4 = await import(
-			/* @vite-ignore */
-			`../../lib/content/${featuredContent.bootcamps[1]}/en/overview`
+		const featuredBootcamp2 = await import(
+			`../../lib/content/bootcamps/${featuredContent.bootcamps[1]}/en/overview.ts`
 		);
-		const courseOverviewFile5 = await import(
-			/* @vite-ignore */
-			`../../lib/content/${featuredContent.roadmaps[0]}/${params.lang}/overview`
+		const featuredRoadmap1 = await import(
+			`../../lib/content/roadmaps/${featuredContent.roadmaps[0]}/${params.lang}/overview.ts`
 		);
-		const courseOverviewFile6 = await import(
-			/* @vite-ignore */
-			`../../lib/content/${featuredContent.roadmaps[1]}/${params.lang}/overview`
+		const featuredRoadmap2 = await import(
+			`../../lib/content/roadmaps/${featuredContent.roadmaps[1]}/${params.lang}/overview.ts`
 		);
 		return {
-			courses: [courseOverviewFile.overview, courseOverviewFile2.overview],
-			bootcamps: [courseOverviewFile3.overview, courseOverviewFile4.overview],
-			roadmaps: [courseOverviewFile5.overview, courseOverviewFile6.overview]
+			courses: [featuredCourse1.overview, featuredCourse2.overview],
+			bootcamps: [featuredBootcamp1.overview, featuredBootcamp2.overview],
+			roadmaps: [featuredRoadmap1.overview, featuredRoadmap2.overview]
 		};
 	} catch (e) {
 		throw new Error();
