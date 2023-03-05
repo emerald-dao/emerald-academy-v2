d3f8cd2f9504366b95e1bcb713e21330d88cd19e
Jacob <jacobtucker818@gmail.com>
2023-03-05 14:42:08 -0500

    fix links for bootcamps

--- src/lib/content/bootcamps/beginner-cadence/en/overview.ts
+++ src/lib/content/bootcamps/beginner-cadence/en/overview.ts
@@ -7,18 +7,18 @@ export const overview: BootcampOverview = {
 	title: 'Beginner Cadence',
 	contentType: ContentTypeEnum.Bootcamp,
 	excerpt:
-		'Learn Cadence. During this bootcamp, you will learn ALL of the major aspects of the language. In addition to writing & deploying your own NFT Contract during the course, you will have the knowledge to write your own contracts too.',
+		'Learn Cadence. During this bootcamp, you will learn ALL of the major aspects of the language. In addition to writing & deploying your own NFT Contract during the bootcamp, you will have the knowledge to write your own contracts too.',
 	metadata: {
 		expertise: ExpertiseEnum.Beginner,
-		duration: '5 chapters',
-		prerequisites: ['javascript'],
-		subjects: [SubjectsEnum.DApp],
+		duration: '4 weeks',
+		prerequisites: [],
+		subjects: [SubjectsEnum.Cadence],
 		price: 'Free',
 		faqs: [
 			{
-				question: '📖 What is this course?',
+				question: '📖 What is this bootcamp?',
 				answer:
-					'Learn Cadence. You will explore ALL of the major aspects of the language.<br/><br/>In addition to writing & deploying your own NFT Contract during the course, you will have the knowledge to write your own contracts too.'
+					'Learn Cadence. You will explore ALL of the major aspects of the language.<br/><br/>In addition to writing & deploying your own NFT Contract during the bootcamp, you will have the knowledge to write your own contracts too.'
 			},
 			{
 				question: '🚀 What will I know by the end?',
@@ -31,19 +31,19 @@ export const overview: BootcampOverview = {
 					'You will create your own NFT smart contract deployed to Flow Testnet. You can see an example of that <a href="https://flow-view-source.com/testnet/account/0xfa88aefbb588049d/contract/YoungJacob" target="_blank">here</a>.<br/><br/>However, the foundational skills you learn will allow you to make much more than that.'
 			},
 			{
-				question: '😇 Who is this course meant for?',
+				question: '😇 Who is this bootcamp meant for?',
 				answer:
 					"<ul><li>If you've never coded Cadence before</li><li>If you have coded in Cadence but want to refresh up on some of the concepts</li><li>If you're interested in the language and want to know more</li></ul>"
 			},
 			{
 				question: '📚 How does it work?',
 				answer:
-					"The lessons will be split into 'chapters,' and chapters will be split into 'lessons.' I did it this way so you can manage your time and progress through the course as you like. Do not feel pressured to do this every lesson. Do it at your own pace and have fun with it! It will always be here :)<br/><br/>Here's an overview:<ul><li>Lessons split into lessons</li><li>Written & video content associated with each lesson. The videos will be from my <a href='https://www.youtube.com/channel/UCf6DzMRwj7SJ3nPrZqd5hHw' target='_blank'>YouTube Channel</a>.</li><li>Quests to complete</li><li>Short quizzes</li><li>A completion certificate (<a href='https://floats.city/jacob.find/event/241503135' target='_blank'>this FLOAT</a>) if you have completed all the quests from Chapters 1-5 (Chapter 6 is extra credit). This will be proof that you have significant Cadence knowledge, and you will be able to show it to whoever you like as proof of expertise. Or just for fun! :D</li></ul>"
+					"The lessons will be split into 'chapters,' and chapters will be split into 'lessons.' I did it this way so you can manage your time and progress through the bootcamp as you like. Do not feel pressured to do this every lesson. Do it at your own pace and have fun with it! It will always be here :)<br/><br/>Here's an overview:<ul><li>Lessons split into lessons</li><li>Written & video content associated with each lesson. The videos will be from my <a href='https://www.youtube.com/channel/UCf6DzMRwj7SJ3nPrZqd5hHw' target='_blank'>YouTube Channel</a>.</li><li>Quests to complete</li><li>Short quizzes</li><li>A completion certificate (<a href='https://floats.city/jacob.find/event/241503135' target='_blank'>this FLOAT</a>) if you have completed all the quests from Chapters 1-5 (Chapter 6 is extra credit). This will be proof that you have significant Cadence knowledge, and you will be able to show it to whoever you like as proof of expertise. Or just for fun! :D</li></ul>"
 			},
 			{
 				question: '❓ What do I need ahead of time?',
 				answer:
-					'Nothing.<br/><br/>In order to complete this course, you need to know absolutely nothing about coding. I included Chapter 1.5 that teaches you the basics of computer programming.<br/><br/><i>Although Chapter 1.5 is a great introduction to programming, if you have never written code before, you may also want to do some outside learning before this course, because it can get complicated very quick. I would suggest some intro to Javascript tutorials to teach you some foundational elements of programming.<br/><br/>It is impossible to learn how to code by watching tutorials/reading articles. You must mess up on your own to make true progress.</i>'
+					'Nothing.<br/><br/>In order to complete this bootcamp, you need to know absolutely nothing about coding. I included Chapter 1.5 that teaches you the basics of computer programming.<br/><br/><i>Although Chapter 1.5 is a great introduction to programming, if you have never written code before, you may also want to do some outside learning before this bootcamp, because it can get complicated very quick. I would suggest some intro to Javascript tutorials to teach you some foundational elements of programming.<br/><br/>It is impossible to learn how to code by watching tutorials/reading articles. You must mess up on your own to make true progress.</i>'
 			},
 			{
 				question: '📁 What are quests?',
@@ -63,42 +63,50 @@ export const overview: BootcampOverview = {
 			{
 				question: '🇺🇸 Multiple languages?',
 				answer:
-					'This course is currently available in English (README.md), Chinese (README_ZH.md), Spanish (README_ES.md), and Russian (README_RU.md). Under each lesson of content, you will see a matching README file for your language of choice.'
+					'This bootcamp is currently available in English (README.md), Chinese (README_ZH.md), Spanish (README_ES.md), and Russian (README_RU.md). Under each lesson of content, you will see a matching README file for your language of choice.'
 			}
 		]
 	},
 	videos: [
 		{
 			name: 'Workshop #1',
-			excerpt: 'Chapters 1-3',
+			excerpt: 'Chapters 1.1 - 2.2',
 			link: 'https://www.youtube/test',
 			subjects: [SubjectsEnum.Cadence],
 			date: new Date(2023, 0, 20, 13, 30),
 			chapters: [
 				{
-					name: 'Chapter 1, Lesson 1 - Introduction to Flow',
-					link: '/catalog/courses/beginner-cadence/chapter1/lesson1.md	'
+					name: 'Chapter 1, Lesson 1',
+					link: '/catalog/courses/beginner-cadence/chapter1/lesson1'
 				},
 				{
-					name: 'Chapter 2, Lesson 1 - Introduction to Flow',
-					link: '/catalog/courses/beginner-cadence/chapter1/lesson2.md'
+					name: 'Chapter 1, Lesson 2',
+					link: '/catalog/courses/beginner-cadence/chapter1/lesson2'
+				},
+				{
+					name: 'Chapter 2, Lesson 1',
+					link: '/catalog/courses/beginner-cadence/chapter2/lesson1'
+				},
+				{
+					name: 'Chapter 2, Lesson 2',
+					link: '/catalog/courses/beginner-cadence/chapter2/lesson2'
 				}
 			]
 		},
 		{
 			name: 'Workshop #2',
-			excerpt: 'Loremn ipsum',
+			excerpt: 'Chapters 2.3 - 2.4',
 			link: 'https://www.youtube/test',
 			subjects: [SubjectsEnum.Webdev],
 			date: new Date(2023, 1, 17, 15, 30),
 			chapters: [
 				{
-					name: 'Chapter 1, Lesson 2 - Introduction to Flow',
-					link: '/catalog/courses/beginner-cadence/chapter2/lesson1.md'
+					name: 'Chapter 2, Lesson 3',
+					link: '/catalog/courses/beginner-cadence/chapter2/lesson3'
 				},
 				{
-					name: 'Chapter 2, Lesson 2 - Introduction to Flow',
-					link: '/catalog/courses/beginner-cadence/chapter2/lesson2.md'
+					name: 'Chapter 2, Lesson 4',
+					link: '/catalog/courses/beginner-cadence/chapter2/lesson4'
 				}
 			]
 		}
