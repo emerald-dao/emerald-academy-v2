9b09d9243c0c11ec5e0f94720121b08f96f9205d
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-03-15 12:26:15 -0300

    Buttons in courses to previous and next lesson

--- src/routes/[lang=lang]/catalog/courses/[name]/[chapter]/[lesson]/+page.svelte
+++ src/routes/[lang=lang]/catalog/courses/[name]/[chapter]/[lesson]/+page.svelte
@@ -1,9 +1,13 @@
 <script type="ts">
 	import { page } from '$app/stores';
-	import type { CourseContents, CourseOverview } from '$lib/types/content/course.interface';
-	import { firstCapital } from '$lib/utilities/dataTransformation/firstCapital';
+	import type {
+		ChapterMetadata,
+		CourseContents,
+		CourseOverview
+	} from '$lib/types/content/course.interface';
 	import { transformUrlToHeading } from '$lib/utilities/dataTransformation/transformUrlToHeading';
 	import { Breadcrumbs } from '@emerald-dao/component-library';
+	import Icon from '@iconify/svelte';
 
 	interface Data {
 		content: ConstructorOfATypedSvelteComponent;
@@ -28,6 +32,25 @@
 			label: transformUrlToHeading($page.params.name)
 		}
 	];
+
+	let allContents: {
+		slug: string;
+		metadata: ChapterMetadata;
+	}[] = [];
+
+	Object.values(data.contents).map((chapter) => {
+		chapter.contents.map((lesson) => {
+			allContents.push(lesson);
+		});
+	});
+	$: previousExample = allContents[findExampleIndex - 1];
+	$: nextExample = allContents[findExampleIndex + 1];
+
+	$: findExampleIndex = allContents.findIndex(
+		(obj) =>
+			obj.slug ===
+			`${$page.params.lang}/catalog/courses/${$page.params.name}/${$page.params.chapter}/${$page.params.lesson}`
+	);
 </script>
 
 <section>
@@ -35,10 +58,76 @@
 	<article>
 		<svelte:component this={data.content} />
 	</article>
+	<div class="adjacents-wrapper">
+		{#if previousExample}
+			<a class="step-back each-adjacent-wrapper" href={`/${previousExample.slug}`}>
+				<Icon icon="tabler:arrow-left" style="color: var(--clr-text-main);" />
+				<div class="left-wrapper column-2">
+					<p class="heading w-medium">
+						{previousExample.metadata.title}
+					</p>
+				</div>
+			</a>
+		{/if}
+		{#if nextExample}
+			<a class="step-next each-adjacent-wrapper" href={`/${nextExample.slug}`}>
+				<div class="column-2">
+					<p class="heading w-medium">
+						{nextExample.metadata.title}
+					</p>
+				</div>
+				<Icon icon="tabler:arrow-right" style="color: var(--clr-text-main);" />
+			</a>
+		{/if}
+	</div>
 </section>
 
 <style type="scss">
 	article {
 		margin-top: var(--space-6);
+	}
+	.adjacents-wrapper {
+		display: flex;
+		flex-direction: column;
+		margin-top: var(--space-11);
+
+		@include mq(medium) {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			grid-template-areas: 'start end';
+			gap: var(--space-4);
+		}
+
+		a {
+			text-decoration: none;
+		}
+
+		.step-back {
+			grid-area: start;
+		}
+
+		.step-next {
+			grid-area: end;
+			margin-top: var(--space-4);
+
+			@include mq(medium) {
+				margin-top: 0;
+			}
+		}
+
+		.each-adjacent-wrapper {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: space-between;
+			gap: var(--space-5);
+			padding: var(--space-4) var(--space-5);
+			border-radius: var(--radius-2);
+			border: var(--border-width-primary) var(--clr-border-primary) solid;
+
+			.left-wrapper {
+				text-align: end;
+			}
+		}
 	}
 </style>
