2fe195ae5c2169477e15bce8c0068e53309bfe7a
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-03-24 18:00:14 -0300

    New gems levels

--- src/routes/[lang=lang]/achievements/+page.svelte
+++ src/routes/[lang=lang]/achievements/+page.svelte
@@ -4,83 +4,85 @@
 	import { achievements } from '$stores/AchievementsTest';
 	import { ProgressBar } from '@emerald-dao/component-library';
 
-	export let data;
+	/* export let data;
 
-	const { value } = data;
+	const { value } = data; */
+
+	let value = 251;
 </script>
 
 <section class="container-small main">
 	<h4>{$LL.ACHIEVEMENTS_H1()}</h4>
 </section>
 <section class="container-small main-diamond-wrapper">
-	{#if value <= 10}
-		{#if value <= 3}
+	{#if value <= 25}
+		{#if value <= 5}
 			<video height="400px" width="auto" autoplay loop muted>
-				<source src="/diamonds/diamond-1-1.mp4" type="video/mp4" />
+				<source src="/diamonds/Green2.webm" type="video/webm" />
 			</video>
-		{:else if value > 3 && value <= 7}
+		{:else if value > 5 && value <= 15}
 			<video height="400px" width="auto" autoplay loop muted>
-				<source src="/diamonds/diamond-1-2.mp4" type="video/mp4" />
+				<source src="/diamonds/Green3.webm" type="video/webm" />
 			</video>
-		{:else if value > 7 && value <= 10}
+		{:else if value > 15 && value <= 25}
 			<video height="400px" width="auto" autoplay loop muted>
-				<source src="/diamonds/diamond-1-3.mp4" type="video/mp4" />
+				<source src="/diamonds/Green4.webm" type="video/webm" />
 			</video>
 		{/if}
 		<h4 class="heading tagline">Diamantis</h4>
 		<ProgressBar
 			{value}
 			min={0}
-			max={10}
-			labelText={`Level 1`}
+			max={25}
+			labelText={`Green Gems - Level ${value}`}
 			width="inherit"
 			showPercentage={true}
 			size="large"
 		/>
-	{:else if value > 10 && value <= 30}
-		{#if value > 10 && value <= 17}
+	{:else if value > 25 && value <= 150}
+		{#if value > 25 && value <= 50}
 			<video height="400px" width="auto" autoplay loop muted>
-				<source src="/diamonds/diamond-2-1.mp4" type="video/mp4" />
+				<source src="/diamonds/Pink2.webm" type="video/webm" />
 			</video>
-		{:else if value > 17 && value <= 24}
+		{:else if value > 50 && value <= 100}
 			<video height="400px" width="auto" autoplay loop muted>
-				<source src="/diamonds/diamond-2-2.mp4" type="video/mp4" />
+				<source src="/diamonds/Pink3.webm" type="video/webm" />
 			</video>
-		{:else if value > 24 && value <= 30}
+		{:else if value > 100 && value <= 150}
 			<video height="400px" width="auto" autoplay loop muted>
-				<source src="/diamonds/diamond-2-3.mp4" type="video/mp4" />
+				<source src="/diamonds/Pink4.webm" type="video/webm" />
 			</video>
 		{/if}
 		<h4 class="heading tagline">Diamantis</h4>
 		<ProgressBar
 			{value}
-			min={11}
-			max={30}
-			labelText={`Level 2`}
+			min={26}
+			max={150}
+			labelText={`Pink Gems - Level ${value}`}
 			width="inherit"
 			showPercentage={true}
 			size="large"
 		/>
-	{:else if value > 30 && value <= 60}
-		{#if value > 30 && value <= 40}
+	{:else if value > 150}
+		{#if value > 150 && value <= 200}
 			<video height="400px" width="auto" autoplay loop muted>
-				<source src="/diamonds/diamond-3-1.mp4" type="video/mp4" />
+				<source src="/diamonds/Purple2.webm" type="video/webm" />
 			</video>
-		{:else if value > 40 && value <= 50}
+		{:else if value > 200 && value <= 250}
 			<video height="400px" width="auto" autoplay loop muted>
-				<source src="/diamonds/diamond-3-2.mp4" type="video/mp4" />
+				<source src="/diamonds/Purple3.webm" type="video/webm" />
 			</video>
-		{:else if value > 50 && value <= 60}
+		{:else if value > 250 && value <= 300}
 			<video height="400px" width="auto" autoplay loop muted>
-				<source src="/diamonds/diamond-3-3.mp4" type="video/mp4" />
+				<source src="/diamonds/Purple4.webm" type="video/webm" />
 			</video>
 		{/if}
 		<h4 class="tagline">Diamantis</h4>
 		<ProgressBar
 			{value}
-			min={31}
-			max={60}
-			labelText={`Level 3`}
+			min={151}
+			max={300}
+			labelText={`Purple Gems - Level ${value}`}
 			width="inherit"
 			showPercentage={true}
 			size="large"
