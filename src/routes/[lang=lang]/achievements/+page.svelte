17849e0ec2bb887cd3a3d6a86e17c7e0f9b48a67
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-03-28 12:43:12 -0300

    My achievements corrections

--- src/routes/[lang=lang]/achievements/+page.svelte
+++ src/routes/[lang=lang]/achievements/+page.svelte
@@ -1,38 +1,34 @@
 <script type="ts">
 	import { LL } from '$i18n/i18n-svelte';
-	import DiamondsCards from '$lib/components/cards/DiamondsCards.svelte';
-	import { achievements } from '$stores/AchievementsTest';
+	import CollectedGem from '$lib/components/cards/CollectedGem.svelte';
+	import UnknownGem from '$lib/components/cards/UnknownGem.svelte';
 	import { ProgressBar } from '@emerald-dao/component-library';
 
 	const greenDiamond: Achievement = {
 		title: 'Green Gem',
-		image: '/diamonds/Green4.webm',
-		description: "You have passed all the levels related to the green gem. Now it's yours!"
+		image: '/diamonds/Green4.webm'
 	};
 
 	const pinkDiamond: Achievement = {
 		title: 'Pink Gem',
-		image: '/diamonds/Pink4.webm',
-		description: "You have passed all the levels related to the pink gem. Now it's yours!"
+		image: '/diamonds/Pink4.webm'
 	};
 
 	const purpleDiamond: Achievement = {
 		title: 'Purple Gem',
-		image: '/diamonds/Purple4.webm',
-		description: "You have passed all the levels related to the purple gem. Now it's yours!"
+		image: '/diamonds/Purple4.webm'
 	};
 
 	interface Achievement {
 		title: string;
 		image: string;
-		description: string;
 	}
 
 	/* export let data;
 
 	const { value } = data; */
 
-	let value = 301;
+	let value = 299;
 </script>
 
 <section class="container-small main">
@@ -53,12 +49,12 @@
 				<source src="/diamonds/Green4.webm" type="video/webm" />
 			</video>
 		{/if}
-		<h4 class="heading tagline">Diamantis</h4>
+		<h4 class="heading tagline">Green Gem</h4>
 		<ProgressBar
 			{value}
 			min={0}
 			max={25}
-			labelText={`Green Gems - Level ${value}`}
+			labelText={`Level ${value}`}
 			width="inherit"
 			showPercentage={true}
 			size="large"
@@ -77,12 +73,12 @@
 				<source src="/diamonds/Pink4.webm" type="video/webm" />
 			</video>
 		{/if}
-		<h4 class="heading tagline">Diamantis</h4>
+		<h4 class="heading tagline">Pink Gem</h4>
 		<ProgressBar
 			{value}
 			min={26}
 			max={150}
-			labelText={`Pink Gems - Level ${value}`}
+			labelText={`Level ${value}`}
 			width="inherit"
 			showPercentage={true}
 			size="large"
@@ -101,12 +97,12 @@
 				<source src="/diamonds/Purple4.webm" type="video/webm" />
 			</video>
 		{/if}
-		<h4 class="tagline">Diamantis</h4>
+		<h4 class="tagline">Purple Gem</h4>
 		<ProgressBar
 			{value}
 			min={151}
 			max={300}
-			labelText={`Purple Gems - Level ${value}`}
+			labelText={`Level ${value}`}
 			width="inherit"
 			showPercentage={true}
 			size="large"
@@ -114,22 +110,54 @@
 	{/if}
 </section>
 
-<section class="container-small diamonds-wrapper">
-	{#if value > 25}
-		<div class="specific-card">
-			<DiamondsCards achievement={greenDiamond} />
-		</div>
-	{/if}
-	{#if value > 150}
-		<div class="specific-card">
-			<DiamondsCards achievement={pinkDiamond} />
-		</div>
-	{/if}
-	{#if value > 300}
-		<div class="specific-card">
-			<DiamondsCards achievement={purpleDiamond} />
-		</div>
-	{/if}
+<section class="container-small gems-collection">
+	<div class="gems-collection-title-wrapper">
+		<h4>Collected gems</h4>
+	</div>
+
+	<div class="diamonds-wrapper">
+		{#if value <= 25}
+			<div class="specific-card">
+				<UnknownGem />
+			</div>
+			<div class="specific-card">
+				<UnknownGem />
+			</div>
+			<div class="specific-card">
+				<UnknownGem />
+			</div>
+		{:else if value > 25 && value <= 150}
+			<div class="specific-card">
+				<CollectedGem achievement={greenDiamond} />
+			</div>
+			<div class="specific-card">
+				<UnknownGem />
+			</div>
+			<div class="specific-card">
+				<UnknownGem />
+			</div>
+		{:else if value > 150 && value < 300}
+			<div class="specific-card">
+				<CollectedGem achievement={greenDiamond} />
+			</div>
+			<div class="specific-card">
+				<CollectedGem achievement={pinkDiamond} />
+			</div>
+			<div class="specific-card">
+				<UnknownGem />
+			</div>
+		{:else if value >= 300}
+			<div class="specific-card">
+				<CollectedGem achievement={greenDiamond} />
+			</div>
+			<div class="specific-card">
+				<CollectedGem achievement={pinkDiamond} />
+			</div>
+			<div class="specific-card">
+				<CollectedGem achievement={purpleDiamond} />
+			</div>
+		{/if}
+	</div>
 </section>
 
 <style type="scss">
@@ -143,7 +171,6 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		margin-bottom: 4rem;
 		gap: var(--space-6);
 		border: var(--clr-border-primary) var(--border-width-primary) solid;
 		border-radius: var(--radius-8);
@@ -153,15 +180,23 @@
 			color: var(--clr-heading-main);
 		}
 	}
+	.gems-collection {
+		.gems-collection-title-wrapper {
+			display: flex;
+			align-items: center;
+			flex-direction: column;
+			margin-bottom: var(--space-14);
+		}
 
-	.diamonds-wrapper {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-around;
-		gap: var(--space-4);
-	}
+		.diamonds-wrapper {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-around;
+			gap: var(--space-4);
+		}
 
-	.specific-card {
-		width: 32%;
+		.specific-card {
+			width: 32%;
+		}
 	}
 </style>
