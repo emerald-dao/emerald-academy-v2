57407fd435f26d0718397557fcb3b1a77902cef0
jacob-tucker <jacobtucker818@gmail.com>
2023-04-06 13:39:58 -0500

    fix description

--- src/lib/content/quickstarts/3-kitty-items/en/overview.ts
+++ src/lib/content/quickstarts/3-kitty-items/en/overview.ts
@@ -15,5 +15,5 @@ export const overview: Template = {
 	},
 	templateRepositoryUrl: 'github.com/onflow/kitty-items',
 	description:
-		"CryptoKitties Sample App is a **complete NFT marketplace** built with [Cadence](https://docs.onflow.org/cadence), Flow's resource-oriented smart contract programming language."
+		"CryptoKitties Sample App is a complete NFT marketplace built with Cadence, Flow's resource-oriented smart contract programming language."
 };
