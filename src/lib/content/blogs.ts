2ed4d1a2b6300ef7d3d4e5f418cb38ff5609352c
RMT Books <121581976+RMTbooks@users.noreply.github.com>
2023-04-14 15:12:20 +0200

    Update blogs.ts

--- src/lib/content/blogs.ts
+++ src/lib/content/blogs.ts
@@ -5,6 +5,22 @@ import { SubjectsEnum } from '$lib/types/content/metadata/subject.enum';
 
 export const blogs: BlogOverview[] = [
 	{
+		image: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1666188602240/0RuRrh87k.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp',
+		title: 'Flow Playground: Getting Started',
+		author: 'Amethyst',
+		authorLink: 'https://amethystcodes.hashnode.dev/',
+		excerpt:
+			'Covers the basics of the Flow Playground, how to deploy a contract and how to interact with deployed contracts using transactions and scripts.',
+		link: 'https://amethystcodes.hashnode.dev/flow-playground-getting-started',
+		contentType: ContentTypeEnum.Blog,
+		metadata: {
+			expertise: ExpertiseEnum.Beginner,
+			duration: '5 minutes',
+			prerequisites: [],
+			subjects: [SubjectsEnum.Cadence]
+		}
+	},
+	{
 		image: 'https://static.wixstatic.com/media/1e611b_17fb3256b319426caef0325494610c15~mv2.png/v1/fill/w_499,h_375,fp_0.50_0.50,q_95,enc_auto/1e611b_17fb3256b319426caef0325494610c15~mv2.png',
 		title: 'Flow Wallets and Account Addresses: Understanding Non-Custodial and Custodial Accounts',
 		author: 'RMT Books',
