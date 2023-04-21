41b32e41f240e26bd53123b26c364778e63c2628
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-04-21 18:27:41 -0300

    LessonVideo component

---
+++ src/lib/components/atoms/LessonVideo.svelte
@@ -0,0 +1,20 @@
+<script type="ts">
+	export let videoId: string;
+</script>
+
+<div>
+	<iframe
+		title="Lesson video"
+		width="100%"
+		height="315"
+		src={`https://www.youtube.com/embed/${videoId}`}
+		frameborder="0"
+		allowfullscreen
+	/>
+</div>
+
+<style type="scss">
+	div {
+		margin-bottom: var(--space-4);
+	}
+</style>
