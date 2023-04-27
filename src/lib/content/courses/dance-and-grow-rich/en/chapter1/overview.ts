a248297039c54532eafb8e96ceefd80cc17404ca
Arun Nadarasa <arun.nadarasa@yahoo.co.uk>
2023-04-27 23:35:00 +0100

    Delete src/lib/content/courses/dance-and-grow-rich/en directory

--- src/lib/content/courses/dance-and-grow-rich/en/chapter1/overview.ts
+++
@@ -1,6 +0,0 @@
-import type { ChapterOverview } from '$lib/types/content/chapter-overview.interface';
-
-export const overview: ChapterOverview = {
-	title: 'Welcome to Dance and Grow Rich',
-	description: 'An introductory video on what to expect from the course.'
-};
--- src/lib/content/courses/dance-and-grow-rich/en/chapter4/overview.ts
+++
@@ -1,6 +0,0 @@
-import type { ChapterOverview } from '$lib/types/content/chapter-overview.interface';
-
-export const overview: ChapterOverview = {
-	title: 'What are NFTs and Cryptocurrency',
-	description: 'To understand the features of NFT and the difference with cryptocurrency.'
-};
