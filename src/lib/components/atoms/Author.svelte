ac5ad4d399ef578faeb2c3253ea37581817582b3
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-05-03 16:30:06 -0300

    Author component to centralize and maintain style

---
+++ src/lib/components/atoms/Author.svelte
@@ -0,0 +1,55 @@
+<script type="ts">
+	import Icon from '@iconify/svelte';
+
+	export let name: string;
+	export let avatarUrl: string | undefined;
+	export let socialMediaUrl: string;
+	export let isVerified: boolean;
+</script>
+
+<div class="row-2">
+	{#if avatarUrl}
+		<img src={avatarUrl} alt="User avatar" />
+	{:else}
+		<img src="/new-avatar.png" alt="Generic avatar" />
+	{/if}
+	<div class="column">
+		<p class="author-word">Author</p>
+		<div class="row-1">
+			<a
+				href={socialMediaUrl}
+				target="_blank"
+				rel="noopener noreferrer"
+				class="header-link row-2 align-center"
+			>
+				<p>{name} ↗</p>
+			</a>
+			{#if isVerified}
+				<div data-tooltip="This author is from Emerald City">
+					<Icon icon="material-symbols:verified-outline-rounded" color="var(--clr-primary-main)" />
+				</div>
+			{/if}
+		</div>
+	</div>
+</div>
+
+<style type="scss">
+	.row-2 {
+		align-items: center;
+
+		img {
+			width: 40px;
+			height: 40px;
+			border-radius: 50%;
+			margin-block: 0;
+		}
+
+		.column {
+			line-height: 100%;
+		}
+
+		.author-word {
+			color: var(--clr-text-off);
+		}
+	}
+</style>
