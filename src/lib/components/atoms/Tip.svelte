599968a951a72ed1d565afbf2df59dc6dce3667d
Jacob Tucker <tsnakejake@aol.com>
2023-05-17 12:08:21 -0500

    tip

--- src/lib/components/atoms/Tip.svelte
+++ src/lib/components/atoms/Tip.svelte
@@ -2,23 +2,6 @@
 	import Icon from '@iconify/svelte';
 
 	export let walletAddress: string;
-	export let name: string;
+	export let authorName: string;
+	export let authorAvatar: string;
 </script>
-
-<svelte:head>
-	<script src="http://localhost:5173/index.js"></script>
-</svelte:head>
-
-<div
-	data-address={walletAddress}
-	data-amount="5.0"
-	data-name="Joe Schmo"
-	data-message="Thank {name} by sending them some $FLOW!"
-	class="flipping-widget-custom"
-	data-bg-color="#12A588"
-	data-size="md"
-	data-tooltip="Tip this author"
->
-	<!-- <Icon icon="material-symbols:attach-money" color="var(--clr-primary-main)" /> -->
-	Click me
-</div>
