bfa17f85abe4030dd207484277f346a3dc0adea0
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-05-18 18:04:21 -0300

    CadenceChallenge page creation

---
+++ src/routes/[lang=lang]/cadence-challenges/+page.svelte
@@ -0,0 +1,11 @@
+<script type="ts">
+	import LL from '$i18n/i18n-svelte';
+	import { ContentTypeEnum } from '$lib/types/content/metadata/content-types.enum';
+</script>
+
+<section class="container">
+	<h1 class="w-medium heading">Cadence Challenges</h1>
+</section>
+
+<style type="scss">
+</style>
