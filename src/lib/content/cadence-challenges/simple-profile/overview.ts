2838399a6ea8989285d913f38644670ef7019c68
Jacob Tucker <tsnakejake@aol.com>
2023-05-24 12:16:16 -0500

    change image

--- src/lib/content/cadence-challenges/simple-profile/overview.ts
+++ src/lib/content/cadence-challenges/simple-profile/overview.ts
@@ -8,6 +8,6 @@ export const overview: CadenceChallenge = {
 	prize: 5,
 	link: 'https://play.flow.com/',
 	description: 'Define a contract that has a Profile resource in it. The Profile must have an id (incremental based on a total supply), name, and address. Allow the owner of the Profile to store it in their account and change their name, and allow public users to read all the information about a users profile.',
-	image: 'https://media.discordapp.net/attachments/1054775421671055390/1105958730799005876/Mateo_Roldos_An_avatar_for_the_Emerald_City_student._Emerald_Ci_0d9c4a87-8861-4779-bd9a-080ef0c838a5.png?width=662&height=662',
+	image: 'https://cdn.discordapp.com/attachments/1054775421671055390/1105958725711319201/tsnakejake_A_cartoon_man_reading_a_mystical_book_with_an_emeral_d5f03067-6692-4152-8ade-37621c0776b5.png',
 	subjects: [SubjectsEnum.Cadence]
 };
