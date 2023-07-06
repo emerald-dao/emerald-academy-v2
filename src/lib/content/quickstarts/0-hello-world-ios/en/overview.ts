712552117ff14bbf134bf5e522dbd7735f7bd678
BoiseITGuru <dev@boiseitguru.com>
2023-07-06 14:41:33 -0600

    twitter handle fix

--- src/lib/content/quickstarts/0-hello-world-ios/en/overview.ts
+++ src/lib/content/quickstarts/0-hello-world-ios/en/overview.ts
@@ -9,7 +9,7 @@ export const overview: Template = {
 	web: 'SwiftUI',
 	author: {
 		name: 'BoiseITGuru',
-		socialMediaUrl: 'https://twitter.com/boiseitguru',
+		socialMediaUrl: 'https://twitter.com/boise_it_guru',
 		avatarUrl:
 			'https://avatars.githubusercontent.com/u/3641594?s=400&u=044fd05bc61270527c4da99212f143595d6fa4a1&v=4',
 		isVerified: true
