35c60bbf2a4b972585ad347a1910940e76b81623
BoiseITGuru <dev@boiseitguru.com>
2023-07-07 12:22:52 -0600

    1-non-fungible-token-ios course

---
+++ src/lib/content/quickstarts/1-non-fungible-token-ios/en/overview.ts
@@ -0,0 +1,21 @@
+import type { Template } from '$lib/types/content/templates.interface';
+import { ContentTypeEnum } from '$lib/types/content/metadata/content-types.enum';
+
+export const overview: Template = {
+	title: 'Non Fungible Token (NFT) - Swift (iOS/iPadOS)',
+	mobile: true,
+	contentType: ContentTypeEnum.Quickstart,
+	audited: true,
+	web: 'SwiftUI',
+	author: {
+		name: 'BoiseITGuru',
+		socialMediaUrl: 'https://twitter.com/boise_it_guru',
+		avatarUrl:
+			'https://avatars.githubusercontent.com/u/3641594?s=400&u=044fd05bc61270527c4da99212f143595d6fa4a1&v=4',
+		isVerified: true,
+		walletAddress: '0x76d988a29af9ea8d'
+	},
+	templateRepositoryUrl: 'github.com/boiseitguru/1-non-fungible-token-ios',
+	description:
+		'A Mobile DApp that lets users create an empty collection, mint some pre-loaded NFTs, and transfer them to another account on Flow testnet.'
+};
