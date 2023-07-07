51a0f359a6ac9e79ec791759184f5685ccc88c1a
BoiseITGuru <dev@boiseitguru.com>
2023-07-07 10:57:44 -0600

    added walletAddress

--- src/lib/content/quickstarts/0-hello-world-ios/en/overview.ts
+++ src/lib/content/quickstarts/0-hello-world-ios/en/overview.ts
@@ -12,7 +12,8 @@ export const overview: Template = {
 		socialMediaUrl: 'https://twitter.com/boise_it_guru',
 		avatarUrl:
 			'https://avatars.githubusercontent.com/u/3641594?s=400&u=044fd05bc61270527c4da99212f143595d6fa4a1&v=4',
-		isVerified: true
+		isVerified: true,
+		walletAddress: '0x76d988a29af9ea8d'
 	},
 	templateRepositoryUrl: 'github.com/boiseitguru/0-hello-world-ios',
 	description: 'A mobile DApp that lets users read and change a greeting field on Flow testnet.'
