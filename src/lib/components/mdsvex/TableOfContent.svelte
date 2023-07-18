77a53c9020f9d6ecd4725fdb1faf61e9156475b2
Jacob <jacobtucker818@gmail.com>
2023-07-18 14:30:25 -0400

    fix quickstart fork link

--- src/lib/components/mdsvex/TableOfContent.svelte
+++ src/lib/components/mdsvex/TableOfContent.svelte
@@ -167,13 +167,7 @@
 				View Code
 			</Button>
 		{:else if contentType === ContentTypeEnum.Quickstart}
-			<Button
-				size="small"
-				color="primary"
-				target="_blank"
-				width="full-width"
-				href={externalUrl + '/fork'}
-			>
+			<Button size="small" color="primary" target="_blank" width="full-width" href={externalUrl}>
 				<Icon icon="tabler:brand-github" />
 				Fork Quickstart
 			</Button>
