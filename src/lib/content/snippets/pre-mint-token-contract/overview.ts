741ae528623c80797f50c74dd417e2ca0f88cdb1
Jacob <jacobtucker818@gmail.com>
2023-07-19 12:54:51 -0400

    fix: add excertp to Opeyemi Oginni

--- src/lib/content/snippets/pre-mint-token-contract/overview.ts
+++ src/lib/content/snippets/pre-mint-token-contract/overview.ts
@@ -5,11 +5,12 @@ import type { SnippetOverview } from '$lib/types/content/snippet.interface';
 export const overview: SnippetOverview = {
     title: 'Token Contract with Pre-Mint Capability',
     contentType: ContentTypeEnum.Snippet,
+    excerpt: 'A FungibleToken Contract that creates an initial supply when it is deployed.',
     author: {
         name: 'Opeyemi Oginni',
         socialMediaUrl: 'https://github.com/OpeOginni',
         avatarUrl: 'https://avatars.githubusercontent.com/u/107570612?v=4',
-        // isVerified: true, // I dont think Im verified lol
+        isVerified: true,
         walletAddress: '0x33d022757904914e'
     },
     metadata: {
