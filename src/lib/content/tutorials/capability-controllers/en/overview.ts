47212fabec4f7df71408c6b64340cf3f7cb8664c
Jacob <jacobtucker818@gmail.com>
2023-08-08 13:29:38 -0400

    feat: cap cons tutorial

---
+++ src/lib/content/tutorials/capability-controllers/en/overview.ts
@@ -0,0 +1,23 @@
+import { ContentTypeEnum } from '$lib/types/content/metadata/content-types.enum';
+import { ExpertiseEnum } from '$lib/types/content/metadata/expertise.enum';
+import { SubjectsEnum } from '$lib/types/content/metadata/subject.enum';
+import type { TutorialOverview } from '$lib/types/content/tutorial.interface';
+
+export const overview: TutorialOverview = {
+    title: 'Capability Controllers (new capability system)',
+    contentType: ContentTypeEnum.Tutorial,
+    author: {
+        name: 'Jacob Tucker',
+        socialMediaUrl: 'https://twitter.com/jacobmtucker',
+        avatarUrl: 'https://avatars.githubusercontent.com/u/15198786?v=4',
+        isVerified: true,
+        walletAddress: '0x99bd48c8036e2876'
+    },
+    excerpt: 'Learn about the new Capability Controllers ("Cap Cons") mechanism. The old `link` and `getCapability` functions are being deprecated.',
+    metadata: {
+        expertise: ExpertiseEnum.Beginner,
+        duration: '20 minutes',
+        prerequisites: [],
+        subjects: [SubjectsEnum.Cadence]
+    }
+};
