905eaf507c86fc09930bfbb89b99a06769088c7e
Jacob <jacobtucker818@gmail.com>
2023-08-09 14:18:25 -0400

    snippet: fcl sign user message on frontend

---
+++ src/lib/content/snippets/fcl-sign-message/overview.ts
@@ -0,0 +1,20 @@
+import { ContentTypeEnum } from '$lib/types/content/metadata/content-types.enum';
+import { SubjectsEnum } from '$lib/types/content/metadata/subject.enum';
+import type { SnippetOverview } from '$lib/types/content/snippet.interface';
+
+export const overview: SnippetOverview = {
+  title: 'Sign a Message with FCL',
+  contentType: ContentTypeEnum.Snippet,
+  author: {
+    name: 'Jacob Tucker',
+    socialMediaUrl: 'https://twitter.com/jacobmtucker',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/15198786?v=4',
+    isVerified: true,
+    walletAddress: '0x99bd48c8036e2876'
+  },
+  excerpt: 'Sign a user message with FCL on the frontend.',
+  metadata: {
+    subjects: [SubjectsEnum.DApp, SubjectsEnum.Javascript]
+  },
+  codeLink: 'https://codesandbox.io/s/fcl-sign-message-tcvhjy?file=/src/index.js'
+};
