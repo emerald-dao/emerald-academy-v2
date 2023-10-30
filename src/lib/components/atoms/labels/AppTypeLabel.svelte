c46c1b47f7d623edac332ccf8a664d2ed188302f
mateoroldos <mateoroldos19@gmail.com>
2023-10-30 18:05:11 -0300

    fix: AppTypeLabel improvements

--- src/lib/components/atoms/labels/AppTypeLabel.svelte
+++ src/lib/components/atoms/labels/AppTypeLabel.svelte
@@ -1,31 +1,13 @@
 <script type="ts">
-	import { AppTypeEnum } from '$lib/types/content/quickstart.interface';
+	import {
+		TECHSTACK_DATA,
+		type QuickstartTechstack
+	} from '$lib/types/content/quickstart.interface';
 	import { Label } from '@emerald-dao/component-library';
 
-	export let type: AppTypeEnum;
-	let tablerIcon: string;
-	let tablerColor: string;
-
-	$: if (type) {
-		if (type === AppTypeEnum.SvelteKit) {
-			tablerIcon = 'tabler:brand-svelte';
-			tablerColor = '#ff4500';
-		} else if (type === AppTypeEnum.NextJS) {
-			tablerIcon = 'tabler:brand-react';
-			tablerColor = '#00abfb';
-		} else {
-			tablerIcon = 'tabler:brand-swift';
-			tablerColor = '#00abfb';
-		}
-	}
+	export let tech: QuickstartTechstack;
 </script>
 
-<Label
-	size="small"
-	color="transparent"
-	iconLeft={tablerIcon}
-	hasBorder={false}
-	iconColor={tablerColor}
->
-	{type}
+<Label size="small" color="transparent" iconLeft={TECHSTACK_DATA[tech].icon} hasBorder={false}>
+	{tech}
 </Label>
