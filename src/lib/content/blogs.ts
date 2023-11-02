6ead22fd1f1250e15d934f49c6d3460ae2520588
Jacob Tucker <tsnakejake@aol.com>
2023-11-02 17:31:31 -0400

    Update blogs.ts

--- src/lib/content/blogs.ts
+++ src/lib/content/blogs.ts
@@ -25,26 +25,6 @@ export const blogs: BlogOverview[] = [
 		}
 	},
 	{
-		image: 'https://static.wixstatic.com/media/1e611b_17fb3256b319426caef0325494610c15~mv2.png/v1/fill/w_499,h_375,fp_0.50_0.50,q_95,enc_auto/1e611b_17fb3256b319426caef0325494610c15~mv2.png',
-		title: 'Flow Wallets and Account Addresses: Understanding Non-Custodial and Custodial Accounts',
-		author: {
-			name: 'RMT Books',
-			socialMediaUrl: 'https://twitter.com/books_rmt',
-			avatarUrl: 'https://pbs.twimg.com/profile_images/1631577606825861121/D7NOiQWx_400x400.jpg',
-			isVerified: true,
-			walletAddress: '0xc67642a8dd697e51'
-		},
-		excerpt: 'The article discusses the difference between wallets and account addresses in the context of digital asset storage and exchange, and explains the concept of custodial and non-custodial accounts, with a focus on the Flow blockchain, including how to create both types of assets, and how collectors and developers use account addresses for different purposes.',
-		link: 'https://www.rmtbooks.com/post/custodial-vs-non-custodial-wallets-on-flow',
-		contentType: ContentTypeEnum.Blog,
-		metadata: {
-			expertise: ExpertiseEnum.Beginner,
-			duration: '5 minutes',
-			prerequisites: [],
-			subjects: []
-		}
-	},
-	{
 		image: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*8Za6QwJWWWuTJbHFaOht9A.jpeg',
 		title: 'Flow CLI Configuration using flow.json',
 		author: {
