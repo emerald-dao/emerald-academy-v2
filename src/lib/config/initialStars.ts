c63d171fc773c92c671b83c1515fe874c8a92613
jacob-tucker <jacobtucker818@gmail.com>
2024-04-15 17:43:09 -0400

    add in initial stars that were from github

--- src/lib/config/initialStars.ts
+++ src/lib/config/initialStars.ts
@@ -1,9 +1,11 @@
 const initialStars: { [key: string]: number } = {
-  'beginner-cadence': 137,
-  'beginner-dapp': 27,
-  'intermediate-cadence': 15
-}
+	'beginner-cadence': 137,
+	'beginner-dapp': 27,
+	'intermediate-cadence': 15,
+	'learn-cadence-beginner-1.0': 137,
+	'learn-cadence-beginner': 137
+};
 
 export function getInitialStars(courseId: string): number {
-  return initialStars[courseId] || 0;
+	return initialStars[courseId] || 0;
 }
